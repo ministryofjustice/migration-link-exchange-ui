843cc73eb0f90a17b621a582cf70fc13c3aa2bea
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-05-16 16:34:39 +0100

    Rename service

--- server/middleware/nunjucks/nunjucksSetup.ts
+++ server/middleware/nunjucks/nunjucksSetup.ts
@@ -10,7 +10,7 @@ export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'Migration Link Exchange'
+  app.locals.applicationName = 'Find a migrated Google file'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
   let assetManifest: Record<string, string> = {}
