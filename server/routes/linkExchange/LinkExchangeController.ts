c1ef77abdbf3255d588eec966f4ce1daf1652c9f
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2025-06-02 12:13:33 +0100

    Add banners for forms and duplicates.

--- server/routes/linkExchange/LinkExchangeController.ts
+++ server/routes/linkExchange/LinkExchangeController.ts
@@ -21,6 +21,41 @@ export default class LinkExchangeController {
     return next()
   }
 
+  private filesHaveDuplicates = (files: any[]) => {
+    if(!files || files.length === 1) {
+      return false;
+    }
+
+    return files.some((file, index) => {
+      // Check if the file name ends in a number in parentheses, e.g. (1), (2), etc.
+      // Or ends in a number in parentheses with a file extension e.g. (1).txt, (2).docx, etc.
+      const match = file.microsoftPath?.match(/\((\d+)\)(\.[a-z]{2,4})?$/);
+      return !!match
+    })
+  }
+
+  private filesHaveLinkToMsFormsRoot = (files: any[]) => {
+    if(!files?.length) {
+      return false;
+    }
+    console.log(files)
+    return files.some((file) => {
+      // Check if the file's Microsoft URL is the root of MS Forms
+      return file.microsoftUrl === 'https://forms.office.com/'
+    })
+  }
+
+  private updateFilesToOpenInWeb = (files: any[]) => {
+    return files?.map((file) => {
+      if(file.microsoftFileType === 'file' && !file.microsoftUrl.includes('?')) {
+        return {
+          ...file,
+          microsoftUrl: `${file.microsoftUrl}?web=1`,
+        }
+      }
+    });
+  }
+
   private render = async (req: Request, res: Response, next: NextFunction) => {
     const { errors, body } = req
     let files
@@ -40,11 +75,24 @@ export default class LinkExchangeController {
         files = await this.fileInformationService.getFilesBySourceURL(req.body.link)
       }
 
+      const banners = [];
+
+      if(this.filesHaveDuplicates(files)) {
+        banners.push(locale.banners.duplicatesFound);
+      }
+
+      if(this.filesHaveLinkToMsFormsRoot(files)) {
+        banners.push(locale.banners.formFound);
+      }
+
+      files = this.updateFilesToOpenInWeb(files);
+
       return res.render('linkExchange', {
         locale,
         data: {
           form: body,
           files,
+          banners,
         },
       })
     } catch (e) {
